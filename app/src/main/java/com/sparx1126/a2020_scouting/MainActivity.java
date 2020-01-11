e084e407d0499734bd1d338ce95d514c8ed5fc1f
JTMongeon <jtmongeon@gmail.com>
2020-01-11 16:34:38 -0500

    Started directory buttons and linking the to new screens

--- app/src/main/java/com/sparx1126/a2020_scouting/MainActivity.java
+++ app/src/main/java/com/sparx1126/a2020_scouting/MainActivity.java
@@ -1,6 +1,9 @@
 package com.sparx1126.a2020_scouting;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -11,6 +14,14 @@ import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 public class MainActivity extends AppCompatActivity {
+    private Button benchmarking;
+    private Button scouting;
+    private Button checklist;
+    private Button view;
+    private Button bugReport;
+    private Button color;
+    private Button admin;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +36,22 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(navView, navController);
-    }
 
+        benchmarking = findViewById(R.id.benchmarking);
+        scouting = findViewById(R.id.scouting);
+        checklist = findViewById(R.id.checklist);
+        view = findViewById(R.id.view);
+        bugReport = findViewById(R.id.bugReport);
+        color = findViewById(R.id.color);
+        admin = findViewById(R.id.color);
+
+//        benchmarking.setOnClickListener(new android.view.View.OnClickListener(){
+//            @Override
+//            public void OnClick(android.view.View v){
+//            }
+//
+//            }
+//
+//;}
+    }
 }
