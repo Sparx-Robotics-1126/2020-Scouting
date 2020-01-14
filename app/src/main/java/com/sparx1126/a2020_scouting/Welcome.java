ea56cebbf7c6b39717b411b9af344eb634552eda
JTMongeon <jtmongeon@gmail.com>
2020-01-13 19:53:17 -0500

    Started directory buttons and linking the to new screens

---
+++ app/src/main/java/com/sparx1126/a2020_scouting/Welcome.java
@@ -0,0 +1,14 @@
+package com.sparx1126.a2020_scouting;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Welcome extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+    }
+}
