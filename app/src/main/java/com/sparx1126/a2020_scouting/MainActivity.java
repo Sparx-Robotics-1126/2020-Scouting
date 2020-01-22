d91ba9e92f094d6867fbcdb332cb24eddae2bda8
skyler <skylerssparxemail@gmail.com>
2020-01-21 19:48:24 -0500

    BlueAliiace to master

--- app/src/main/java/com/sparx1126/a2020_scouting/MainActivity.java
+++ app/src/main/java/com/sparx1126/a2020_scouting/MainActivity.java
@@ -2,9 +2,6 @@ package com.sparx1126.a2020_scouting;
 
 import android.os.Bundle;
 import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.sparx1126.a2020_scouting.Utilities.BlueAllianceNetwork;
@@ -15,34 +12,11 @@ import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-import org.json.JSONStringer;
-
-import java.io.IOException;
-import java.util.HashMap;
 import java.util.Map;
 
-import okhttp3.Call;
-import okhttp3.Callback;
-import okhttp3.Request;
-import okhttp3.OkHttpClient;
-import okhttp3.Response;
-
 public class MainActivity extends AppCompatActivity {
-    private Button benchmarking;
-    private Button scouting;
-    private Button checklist;
-    private Button view;
-    private Button bugReport;
-    private Button color;
-    private Button admin;
     private BlueAllianceNetwork network = BlueAllianceNetwork.getInstance();
     private Map<String, BlueAllianceEvent> events;
-    String url = "http://www.thebluealliance.com/api/v3/team/frc1126/events/2019";
-    String authKeyHeader = "X-TBA-Auth-Key";
-    String authKey = "4EFyOEdszrGNcCJuibGSr6W92SjET2cfhx2QU9Agxv3LNASra77KcsCEv5GnoSIq";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -70,12 +44,5 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
-
-
-
-
-
-
     }
-
 }
