97a7018bc7e69694bb397e87e30c3fd2eacef57b
Hiram <jimenez.hiram@gmail.com>
2020-02-19 17:40:30 -0500

    Merge branches 'CheckingMerge' and 'master' of https://github.com/Sparx-Robotics-1126/2020-Scouting into CheckingMerge
    # Conflicts:
    #	app/src/main/res/values/strings.xml

--- app/src/main/java/com/sparx1126/a2020_scouting/Welcome.java
+++ app/src/main/java/com/sparx1126/a2020_scouting/Welcome.java
@@ -132,7 +132,7 @@ public class Welcome extends AppCompatActivity {
         //Temporary
         editor.putString(getString(R.string.EMAIL), "sparx1126scouts@gmail.com");
         editor.putString(getString(R.string.PASSWORD), "gosparx!");
-        editor.putString(getString(R.string.TEAM), "1126");
+        editor.putString(getString(R.string.TEAM), "58"); // participated in Week 0
         editor.putBoolean(getString(R.string.SCOUT), true);
         editor.apply();
 
